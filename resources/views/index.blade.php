c7dcfa77adc61eddc002dda29f9d6bb1fcc93371
emedranoh19a <emedranoh19a@hotmail.com>
2021-10-02 21:35:26 +0900

    Visualise blog

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+     <head>
+    <meta charset="utf-8">
+    <title>Blog</title>
+  </head>
+  <body>
+    <h1>This is the blog title</h1>
+    <div class="posts">
+      <div class="post">
+        <h2 class="title"> Title 1 </h2>
+        <p class="body">This is a simple body</p>
+      </div>
+    </div>
+    this is a simple text
+  </body>
+</html>
